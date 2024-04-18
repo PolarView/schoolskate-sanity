06891e19fe1610bda385949af35cfadd1dc10227
Sanchiz <chrisrosy29@mail.ru>
2024-04-18 20:10:34 +0300

    6

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,29 +1,8 @@
 export const dynamic = "force-dynamic";
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+import next from "next";
+import { NextResponse } from "next/server";
 
-export default async function POST(req, res) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME];
-  const isValid = isValidSignature(JSON.stringify(req.body), signature, SANITY_WEBHOOK_SECRET);
-
-  console.log(`===== Is the webhook request valid? ${isValid}`);
-
-  // Validate signature
-  if (!isValid) {
-    res.status(401).json({ success: false, message: "Invalid signature" });
-    return;
-  }
-
-  try {
-    const pathToRevalidate = req.body.slug.current;
-
-    console.log(`===== Revalidating: ${pathToRevalidate}`);
-
-    await res.revalidate("/");
-
-    return res.json({ revalidated: true });
-  } catch (err) {
-    // Could not revalidate. The stale page will continue to be shown until
-    // this issue is fixed.
-    return res.status(500).send("Error while revalidating");
-  }
+export async function POST(request) {
+  return NextResponse.json({ pass: 123 });
 }
