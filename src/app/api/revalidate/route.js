e7fde4a8980bc69a841930fa4c8c3d03086ac60a
Sanchiz <chrisrosy29@mail.ru>
2024-04-18 20:24:32 +0300

    8

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,31 +1,36 @@
 export const dynamic = "force-dynamic";
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+import { NextResponse, NextNextRequestuest } from "next/server";
 
 const SANITY_WEBHOOK_SECRET = process.env.SANITY_WEBHOOK_SECRET;
 
-export async function POST(req, res) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME];
-  const isValid = isValidSignature(JSON.stringify(req.body), signature, SANITY_WEBHOOK_SECRET);
+export async function POST() {
+  const signature = NextRequest.headers[SIGNATURE_HEADER_NAME];
+  const isValid = isValidSignature(
+    JSON.stringify(NextRequest.body),
+    signature,
+    SANITY_WEBHOOK_SECRET
+  );
 
-  console.log(`===== Is the webhook request valid? ${isValid}`);
+  console.log(`===== Is the webhook NextRequestuest valid? ${isValid}`);
 
   // Validate signature
   if (!isValid) {
-    res.status(401).json({ success: false, message: "Invalid signature" });
+    NextResponse.status(401).json({ success: false, message: "Invalid signature" });
     return;
   }
 
   try {
-    const pathToRevalidate = req.body.slug.current;
+    const pathToRevalidate = NextRequest.body.slug.current;
 
     console.log(`===== Revalidating: ${pathToRevalidate}`);
 
-    await res.revalidate("/");
+    await NextResponse.revalidate("/");
 
-    return res.json({ revalidated: true });
+    return NextResponse.json({ revalidated: true });
   } catch (err) {
     // Could not revalidate. The stale page will continue to be shown until
     // this issue is fixed.
-    return res.status(500).send("Error while revalidating");
+    return NextResponse.status(500).send("Error while revalidating");
   }
 }
